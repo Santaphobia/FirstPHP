264962f76ab050cb4886ce89cbd52a2cb5cffa30
Santaphobia <72969670+Santaphobia@users.noreply.github.com>
2022-11-05 20:35:04 +0300

    delete old code

--- public/index.php
+++ public/index.php
@@ -9,27 +9,7 @@ include realpath("../engine/Autoload.php");
 
 
 try {
-
-App::call()->run($config);
-//$product = new \app\models\entities\Product('f','ff',232323, 'fffff');
-//var_dump($product);
-//App::call()->productRepository->save($product);
-//$login = $_SESSION['login'];
-//$basket = App::call()->basketRepository->getAll();
-//$order = new Orders($login, 'ff' , 'fff', 'fff', json_encode($basket));
-//var_dump($order);
-//App::call()->ordersRepository->save($order);
-//die();
-////json_encode($basket) $price = App::call()->productRepository->getPrice(5);
-//var_dump($price);
-////var_dump(App::call()->basketRepository->getSumFieldWhere('quantity', 'session_id', session_id()));
-//$basket = App::call()->basketRepository->getWhereTwo('session_id', session_id(), 'product_id', 5);
-//var_dump($basket);
-//////$basket->quantity += 1;
-//////var_dump($basket);
-//////App::call()->basketRepository->update($basket);
-//die();
-
+    App::call()->run($config);
 } catch (\Exception $e) {
     var_dump($e);
 };
